c1c98f3ac457b4f54766a25399d1564bfa4a8577
LizPinango <lizpinango15@gmail.com>
2024-04-26 17:41:59 -0400

    ejercicios 3.15 al 3.18 de la parte 3 del curso

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -6,8 +6,8 @@ const Person = require('./models/person')
 
 const app = express();
 
-app.use(express.json());
 app.use(express.static('dist'))
+app.use(express.json());
 app.use(cors());
 
 app.use(morgan((tokens, req, res) => [
@@ -19,32 +19,11 @@ app.use(morgan((tokens, req, res) => [
   JSON.stringify(req.body),
 ].join(' ')));
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
-
-app.get('/info', (req, res) => {
+app.get('/info', (req, res) => {//Conecta con la DB
   const date = new Date();
-  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`);  
+  Person.find({}).then((persons) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`);
+  });  
 })
 
 app.get('/api/persons', (req, res) => { //conecta con la BD 
@@ -53,22 +32,25 @@ app.get('/api/persons', (req, res) => { //conecta con la BD
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {    
-  const id = Number(req.params.id);
-  const person = persons.find(p => p.id === id);
-  
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/persons/:id', (req, res, next) => {//Conecta con la DB  
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(p => p.id !== id);
-
-  res.status(204).end()
+//Conecta con la DB - Error Handler 
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {//Conecta con la DB
@@ -95,6 +77,35 @@ app.post('/api/persons', (req, res) => {//Conecta con la DB
     res.json(savedPerson);
   });
 })
+
+//Conecta con la DB - Error Handler
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+//Error Handler Middleware 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
