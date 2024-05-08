d01a3afef28811c94b84441c8df3593068ecbd97
LizPinango <lizpinango15@gmail.com>
2024-05-08 16:50:04 -0400

    creado el archivo app.js

--- Part4/bloglist/index.js
+++ Part4/bloglist/index.js
@@ -1,31 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
+const app = require('./app')
 const logger = require('./utils/logger'); //the console.logs
 const config = require('./utils/config'); //env variables 
 
-const blogsRouter = require('./controllers/blogs'); //rutas
-
-const mongoUrl = config.MONGODB_URI
-
-logger.info('connecting to', mongoUrl)
-
-mongoose.connect(mongoUrl)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch(error => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
