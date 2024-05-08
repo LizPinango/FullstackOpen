65530aeffc9b77382139e9db5957b33270e2b8f3
LizPinango <lizpinango15@gmail.com>
2024-05-08 16:40:05 -0400

    cradas las carpetas controllers y models

--- Part4/bloglist/index.js
+++ Part4/bloglist/index.js
@@ -6,22 +6,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger'); //the console.logs
 const config = require('./utils/config'); //env variables 
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/blogs'); //rutas
 
 const mongoUrl = config.MONGODB_URI
 
@@ -38,21 +23,7 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
