d29915ed828a519ba22e24b528e1ad82d13d1e57
LizPinango <lizpinango15@gmail.com>
2024-05-11 16:30:21 -0400

    creado el archivo middleware.js en la carpeta utils

--- Part4/bloglist/app.js
+++ Part4/bloglist/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 
 const logger = require('./utils/logger'); //the console.logs
 const config = require('./utils/config'); //env variables 
-
+const middleware = require('./utils/middleware'); // middleware
 const blogsRouter = require('./controllers/blogs'); //rutas
 
 mongoose.set('strictQuery', false)
@@ -22,7 +22,10 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger) // request info
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.errorHandler) // Handler errors 
 
 module.exports = app
