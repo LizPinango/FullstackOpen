a62288f3273e6dda90f15ad6df66eb4c19e9ca93
LizPinango <lizpinango15@gmail.com>
2024-05-18 16:55:36 -0400

    ejercicio 4.5 de la parte 4 del curso

--- Part4/bloglist/utils/list_helper.js
+++ Part4/bloglist/utils/list_helper.js
@@ -9,9 +9,23 @@ const totalLikes = (blogs) => {
 
   return blogs.reduce(reducer, 0)
 }
+
+const favoriteBlog = (blogs) => {
+  let mostLikes = 0; 
+  let mostLikesIndex = 0; 
+  const length = blogs.length;
+  for (let i = 0; i<length; i++){       
+    if(mostLikes<blogs[i].likes){      
+      mostLikes = blogs[i].likes;
+      mostLikesIndex = i;
+    }
+  }
+
+  return blogs[mostLikesIndex];
+}
   
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
 
 
